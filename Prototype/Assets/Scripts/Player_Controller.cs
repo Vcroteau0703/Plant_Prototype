b1bf444638707bc8847f563e9031e1d3fd3c1752
Tanner Cobb <tannercobb359@gmail.com>
2022-01-31 16:27:24 -0500

    Movement Update 02
    - Added custom jumping
    - Added air control
    - Added movement settings
    - Added Physics material to player
    - Added Wall Jumping (in progress)

--- Prototype/Assets/Scripts/Player_Controller.cs
+++ Prototype/Assets/Scripts/Player_Controller.cs
@@ -7,17 +7,21 @@ public class Player_Controller : MonoBehaviour
 {
     //Class References
     private Controls controls;
-    public Rigidbody rb;
-    public CapsuleCollider col;
+    private Rigidbody rb;
+    private CapsuleCollider col;
+
 
     [Header("Movement Settings")]
     public float move_speed = 1.0f;
     public float move_smoothing = 0.5f;
     public float jump_power = 1.0f;
-    public float force, time, floatiness;
+    public float force, time, floatiness, weight;
+    public float air_control = 1.0f;
+    public float max_air_speed = 10f;
 
     private Vector2 direction;
-    public bool grounded;
+    private bool grounded;
+    private bool[] wall = new bool[2];
 
     private void Awake()
     {
@@ -30,6 +34,7 @@ public class Player_Controller : MonoBehaviour
         controls.Player.Enable();
     }
 
+    #region MOVEMENT/AIR CONTROL
     private void Request_Movement(InputAction.CallbackContext context)
     {
         direction = context.ReadValue<Vector2>();
@@ -37,9 +42,19 @@ public class Player_Controller : MonoBehaviour
 
     private void Movement()
     {
-        rb.velocity = Vector3.Lerp(rb.velocity, new Vector3(direction.x * move_speed, rb.velocity.y, 0), move_smoothing);
+        if(grounded == false) 
+        {
+            float air_speed = Mathf.Clamp(Mathf.Lerp(rb.velocity.x, rb.velocity.x + direction.x * move_speed, air_control), -max_air_speed, max_air_speed);
+            rb.velocity = Vector3.Lerp(rb.velocity, new Vector3(air_speed, rb.velocity.y, 0), move_smoothing);
+        }
+        else
+        {
+            rb.velocity = Vector3.Lerp(rb.velocity, new Vector3(direction.x * move_speed, rb.velocity.y, 0), move_smoothing);
+        }
     }
+    #endregion
 
+    #region JUMP
     private void Jump(InputAction.CallbackContext context)
     {
         if (grounded == true)
@@ -50,25 +65,47 @@ public class Player_Controller : MonoBehaviour
 
     private IEnumerator Jump()
     {
-        Debug.Log("Jump");
+        rb.AddForce(Vector3.up * jump_power, ForceMode.Force);
         time = 0;
         while (time != 10f)
         {
             time += Time.deltaTime;
-            force = Mathf.Pow(-time, 2)/ floatiness * (Mathf.Pow(time, 2) / floatiness) + jump_power;
+            force = (-Mathf.Pow(time, 2) / (1/weight*10)) + floatiness;
             rb.AddForce(new Vector3(0,force,0), ForceMode.Acceleration);
             yield return new WaitForEndOfFrame();
-            if(force < 0) { break; }
+            if(grounded) { force = 0; break; }
         }
     }
+    #endregion
 
-    private bool Ground_Check()
+    #region WALL JUMP
+
+    public void Wall_Jump()
+    {
+
+    }
+
+    #endregion
+
+    #region UTILITY
+    private void Ground_Check()
     {
         Vector3 pos = (transform.position - (Vector3.up * (col.bounds.size.y / 2)) + (Vector3.down * 0.02f));
-        Collider[] floor = Physics.OverlapBox(pos, new Vector3(0.5f, 0.01f, 0.5f));
-        if(floor.Length > 0) { return true; }
-        else{return false;}
+        Collider[] floor = Physics.OverlapBox(pos, new Vector3(0.2f, 0.01f, 0.2f));
+        grounded = floor.Length > 0 ? true : false;
     }
+
+    private void Wall_Check()
+    {
+        Vector3 posA = (transform.position - (Vector3.left * (col.bounds.size.x / 2)) + (Vector3.left * 0.7f));
+        Collider[] wall_left = Physics.OverlapBox(posA, new Vector3(0.01f, 0.2f, 0.2f));
+        wall[0] = wall_left.Length > 0 ? true : false;
+
+        Vector3 posB = (transform.position - (Vector3.right * (col.bounds.size.x / 2)) + (Vector3.right * 0.7f));
+        Collider[] wall_right = Physics.OverlapBox(posB, new Vector3(0.01f, 0.2f, 0.2f));
+        wall[1] = wall_right.Length > 0 ? true : false;
+    }
+    #endregion
 
     private void FixedUpdate()
     {
@@ -77,21 +114,31 @@ public class Player_Controller : MonoBehaviour
 
     private void Update()
     {
-        grounded = Ground_Check();
+        Ground_Check();
+        Wall_Check();
     }
 
     private void OnDisable()
     {
         controls.Player.Disable();
     }
-
     private void OnDrawGizmos()
     {
+        Gizmos.color = Color.blue;
         if (grounded)
         {
-            Gizmos.color = Color.blue;
             Vector3 pos = (transform.position - (Vector3.up * (col.bounds.size.y / 2)) + (Vector3.down * 0.02f));
-            Gizmos.DrawWireCube(pos, new Vector3(0.5f, 0.01f, 0.5f));
+            Gizmos.DrawWireCube(pos, new Vector3(col.bounds.size.x / 2, 0.01f, 0.2f) * 2);
+        }
+        if (wall[0])
+        {         
+            Vector3 pos = (transform.position - (Vector3.left * (col.bounds.size.x / 2)) + (Vector3.left * 0.7f));
+            Gizmos.DrawWireCube(pos, new Vector3(0.01f, col.bounds.size.y/2, 0.2f) * 2);
+        }
+        if (wall[1])
+        {
+            Vector3 pos = (transform.position - (Vector3.right * (col.bounds.size.x / 2)) + (Vector3.right * 0.7f));
+            Gizmos.DrawWireCube(pos, new Vector3(0.01f, col.bounds.size.y/2, 0.2f) * 2);
         }
     }
 }
