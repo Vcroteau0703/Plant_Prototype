6a41c2d5722be62b478e2a31835d435823ad5867
Tanner Cobb <tannercobb359@gmail.com>
2022-02-11 12:37:37 -0500

    UPDATE: Control Settings Changed + Gizmo Display Updated

---
+++ Prototype/Assets/Scripts/Player_Control_Settings.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Player Control Settings", menuName = "Player Control Settings")]
+public class Player_Control_Settings : ScriptableObject
+{
+    #region MOVEMENT VARIABLES
+    [Header("Movement Settings")]
+    public float move_speed = 1.0f;
+    public float move_smoothing = 0.5f;
+    public float downforce = 1f;
+    public LayerMask walkable;
+    [Header("Jump Settings")]
+    public float jump_power = 1.0f;
+    public float floatiness, weight;
+    public float coyote_jump_delay = 0.15f;
+    [Header("Air Control Settings")]
+    public float air_control = 1.0f;
+    public float max_air_speed = 10.0f;
+    public float max_fall_speed = 20.0f;
+    [Header("Wall Jump Settings")]
+    public float wall_jump_power = 10.0f;
+    public float wall_grab_strength = 20.0f;
+    public float wall_jump_angle = 45.0f;
+    public float wall_release_strength = 5.0f;
+    [Header("Wall Slide Settings")]
+    public float landing_slide_duration = 2.0f;
+    public float landing_slide_speed = 5.0f;
+    public float wall_slide_speed = 5.0f;
+    public float wall_slide_smoothing = 0.1f;
+    [Header("Detection Settings")]
+    public float slope_angle = 30f;
+    public float ceiling_angle = 30f;
+    public float min_climb_height = 1f;
+    [Header("Input Settings")]
+    [Range(0f, 100f)] public float horizontal_deadzone = 20f;
+    [Range(0f, 100f)] public float vertical_deadzone = 20f;
+    #endregion
+}
--- Prototype/Assets/Scripts/Player_Controller.cs
+++ Prototype/Assets/Scripts/Player_Controller.cs
@@ -12,49 +12,17 @@ public class Player_Controller : MonoBehaviour
     private Collider col;
     private Animator animator;
 
+    public Player_Control_Settings settings;
     public Bounds Player_Bounds;
+    public bool show_bounds;
 
-    #region MOVEMENT VARIABLES
-    [Header("Movement Settings")]
-    public float move_speed = 1.0f;
-    public float move_smoothing = 0.5f;
-    public float downforce = 1f;
-    public LayerMask walkable;
-    [Header("Jump Settings")]
-    public float jump_power = 1.0f;
-    public float floatiness, weight;   
-    public float coyote_jump_delay = 0.15f;
-    [Header("Air Control Settings")]
-    public float air_control = 1.0f;
-    public float max_air_speed = 10.0f;
-    public float max_fall_speed = 20.0f;
-    [Header("Wall Jump Settings")]
-    public float wall_jump_power = 10.0f;
-    public float wall_grab_strength = 20.0f; 
-    public float wall_jump_angle = 45.0f;
-    public float wall_release_strength = 5.0f;
-    [Header("Wall Slide Settings")]
-    public float landing_slide_duration = 2.0f;
-    public float landing_slide_speed = 5.0f;
-    public float wall_slide_speed = 5.0f;
-    public float wall_slide_smoothing = 0.1f;
-    [Header("Detection Settings")]
-    public float slope_angle = 30f;
-    public float ceiling_angle = 30f;
-    public float min_climb_height = 1f;
-    [Header("Input Settings")]
-    [Range(0f, 100f)] public float horizontal_deadzone = 20f;
-    [Range(0f, 100f)] public float vertical_deadzone = 20f;
-    
     private Vector2 direction;
-    public Vector2 detection;
+    private Vector2 detection;
     public bool isControlling = true;
 
     public enum State { Waiting = default, Grounded, Ceiling, Cling, Aerial }
     public State current_state;
 
-
-    #endregion
     private void Awake()
     {
         instance = this;
@@ -73,8 +41,8 @@ public class Player_Controller : MonoBehaviour
     private void Request_Movement(InputAction.CallbackContext context)
     {       
         Vector2 temp = context.ReadValue<Vector2>();
-        float x =  Mathf.Abs(temp.x) > (horizontal_deadzone / 100f) ? temp.x : 0.0f;
-        float y = Mathf.Abs(temp.y) > (vertical_deadzone / 100f) ? temp.y : 0.0f;      
+        float x =  Mathf.Abs(temp.x) > (settings.horizontal_deadzone / 100f) ? temp.x : 0.0f;
+        float y = Mathf.Abs(temp.y) > (settings.vertical_deadzone / 100f) ? temp.y : 0.0f;      
         direction = new Vector2(x, y);
     }
 
@@ -88,18 +56,18 @@ public class Player_Controller : MonoBehaviour
         {
             default: break;
             case State.Grounded:
-                speed = direction.x * move_speed;
-                rb.velocity = Vector3.Lerp(rb.velocity, new Vector3(speed, rb.velocity.y, 0), move_smoothing);
+                speed = direction.x * settings.move_speed;
+                rb.velocity = Vector3.Lerp(rb.velocity, new Vector3(speed, rb.velocity.y, 0), settings.move_smoothing);
                 break;
             case State.Cling:
-                if(direction.y < 0) { Wall_Slide(wall_slide_speed); }
+                if(direction.y < 0) { Wall_Slide(settings.wall_slide_speed); }
                 if(direction.x != 0) {
-                    speed = direction.x * move_speed * wall_release_strength / 100;
+                    speed = direction.x * settings.move_speed * settings.wall_release_strength / 100;
                     rb.velocity = new Vector3(speed, rb.velocity.y, 0);}
                 break;
             case State.Aerial:
-                speed = Mathf.Clamp(Mathf.Lerp(rb.velocity.x, rb.velocity.x + direction.x * move_speed, air_control), -max_air_speed, max_air_speed);
-                rb.velocity = Vector3.Lerp(rb.velocity, new Vector3(speed, rb.velocity.y, 0), move_smoothing);
+                speed = Mathf.Clamp(Mathf.Lerp(rb.velocity.x, rb.velocity.x + direction.x * settings.move_speed, settings.air_control), -settings.max_air_speed, settings.max_air_speed);
+                rb.velocity = Vector3.Lerp(rb.velocity, new Vector3(speed, rb.velocity.y, 0), settings.move_smoothing);
                 break;
         }
     }
@@ -122,15 +90,15 @@ public class Player_Controller : MonoBehaviour
     {              
         float force;
         rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
-        rb.AddForce(Vector3.up * jump_power, ForceMode.Force);
+        rb.AddForce(Vector3.up * settings.jump_power, ForceMode.Force);
         float time = 0;
         while (time >= 0)
         {
             time += Time.deltaTime;
-            force = -Mathf.Pow(time, 2) / (1/weight*10) + floatiness;
-            if (controls.Player.Jump.phase == InputActionPhase.Waiting) { force = -Mathf.Abs(force) * downforce; }
-            if (rb.velocity.y > -max_fall_speed) { rb.AddForce(Vector3.up * force, ForceMode.Acceleration); };
-            rb.velocity = new Vector3(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -max_fall_speed, float.PositiveInfinity), rb.velocity.z);
+            force = -Mathf.Pow(time, 2) / (1/ settings.weight *10) + settings.floatiness;
+            if (controls.Player.Jump.phase == InputActionPhase.Waiting) { force = -Mathf.Abs(force) * settings.downforce; }
+            if (rb.velocity.y > -settings.max_fall_speed) { rb.AddForce(Vector3.up * force, ForceMode.Acceleration); };
+            rb.velocity = new Vector3(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -settings.max_fall_speed, float.PositiveInfinity), rb.velocity.z);
             yield return new WaitForEndOfFrame();
             if (time > 0.2 && current_state != State.Aerial) { break; }
         }
@@ -142,9 +110,9 @@ public class Player_Controller : MonoBehaviour
     {
         isControlling = false;
         rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
-        float angle = wall_jump_angle * detection.x;
+        float angle = settings.wall_jump_angle * detection.x;
         Quaternion rotA = Quaternion.AngleAxis(angle, Vector3.forward);
-        rb.AddForce((rotA * Vector3.up) * wall_jump_power, ForceMode.Force);
+        rb.AddForce((rotA * Vector3.up) * settings.wall_jump_power, ForceMode.Force);
         Debug.Log((rotA * Vector3.up));
         yield return new WaitForSeconds(0.15f);       
         isControlling = true;
@@ -154,10 +122,10 @@ public class Player_Controller : MonoBehaviour
         while (time >= 0)
         {
             time += Time.deltaTime;
-            force = -Mathf.Pow(time, 2) / (1 / weight * 10) + floatiness;
-            if (controls.Player.Jump.phase == InputActionPhase.Waiting) { force = -Mathf.Abs(force) * downforce; }
-            if (rb.velocity.y > -max_fall_speed) { rb.AddForce(Vector3.up * force, ForceMode.Acceleration); };
-            rb.velocity = new Vector3(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -max_fall_speed, float.PositiveInfinity), rb.velocity.z);
+            force = -Mathf.Pow(time, 2) / (1 / settings.weight * 10) + settings.floatiness;
+            if (controls.Player.Jump.phase == InputActionPhase.Waiting) { force = -Mathf.Abs(force) * settings.downforce; }
+            if (rb.velocity.y > -settings.max_fall_speed) { rb.AddForce(Vector3.up * force, ForceMode.Acceleration); };
+            rb.velocity = new Vector3(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -settings.max_fall_speed, float.PositiveInfinity), rb.velocity.z);
             yield return new WaitForEndOfFrame();
             if (current_state != State.Aerial) { break; }
         }
@@ -168,12 +136,12 @@ public class Player_Controller : MonoBehaviour
         rb.useGravity = false;
         Flip(Mathf.RoundToInt(detection.x));
         Vector2 dir = transform.position - (transform.position - (Vector3)detection);
-        rb.AddForce(dir * wall_grab_strength, ForceMode.Force);
-        if (slide_time < landing_slide_duration){slide_time += Time.deltaTime; Wall_Slide(landing_slide_speed);}        
+        rb.AddForce(dir * settings.wall_grab_strength, ForceMode.Force);
+        if (slide_time < settings.landing_slide_duration){slide_time += Time.deltaTime; Wall_Slide(settings.landing_slide_speed);}        
     }
     public void Wall_Slide(float speed)
     {        
-        transform.position = Vector2.Lerp(transform.position, (Vector2)transform.position + Vector2.down * (speed / 10), wall_slide_smoothing);
+        transform.position = Vector2.Lerp(transform.position, (Vector2)transform.position + Vector2.down * (speed / 10), settings.wall_slide_smoothing);
     }
     #endregion
 
@@ -190,7 +158,7 @@ public class Player_Controller : MonoBehaviour
     {
         animator.SetFloat("Speed_X", Mathf.Abs((int)((direction.x % 1) + (direction.x / 1))));
         animator.SetFloat("Speed_Y", rb.velocity.y);
-        animator.speed = rb.velocity.magnitude / max_air_speed;
+        animator.speed = rb.velocity.magnitude / settings.max_air_speed;
         switch (current_state)
         {
             case State.Grounded:
@@ -240,9 +208,28 @@ public class Player_Controller : MonoBehaviour
         }
     }
 
+    private IEnumerator Delay_State(State state, float t)
+    {
+        while(t > 0 && rb.velocity.y <= 0)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
+            t -= Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        current_state = state;
+        rb.useGravity = true;
+        slide_time = 0;
+        detection = Vector3.zero;
+    }
+
 
     private void OnCollisionExit(Collision collision)
     {
+        if(last_state == State.Grounded && rb.velocity.y < 0)
+        {
+            StartCoroutine(Delay_State(State.Aerial, settings.coyote_jump_delay));
+            return;
+        }
         current_state = State.Aerial;
         rb.useGravity = true;
         slide_time = 0;
@@ -256,11 +243,10 @@ public class Player_Controller : MonoBehaviour
         float angle = Vector3.Angle(dir, -transform.up);
         detection = dir;
         float height = collision.GetContact(0).otherCollider.bounds.size.y;
-        Debug.Log("height: " + height);
 
-        if (angle < slope_angle) { current_state = State.Grounded; } // GROUND
-        else if(angle > 180 - ceiling_angle) { current_state = State.Ceiling; } // CEILING
-        else if(angle > slope_angle && angle < 180 - ceiling_angle && height > min_climb_height) { current_state = State.Cling; Wall_Grab(); } // WALL 
+        if (angle < settings.slope_angle) { current_state = State.Grounded; } // GROUND
+        else if(angle > 180 - settings.ceiling_angle) { current_state = State.Ceiling; } // CEILING
+        else if(angle > settings.slope_angle && angle < 180 - settings.ceiling_angle && height > settings.min_climb_height) { current_state = State.Cling; Wall_Grab(); } // WALL 
         else { current_state = State.Waiting; } // WAITING (DEFAULT)
 
         Debug.DrawLine(transform.position, transform.position + (p - transform.position).normalized, Color.green);
@@ -281,25 +267,35 @@ public class Player_Controller : MonoBehaviour
     {
         Gizmos.color = Color.yellow;
         #region SLOPE ANGLE BOUNDS
-        Quaternion rot = Quaternion.AngleAxis(slope_angle, Vector3.forward);
-        Quaternion rot2 = Quaternion.AngleAxis((-slope_angle), Vector3.forward);
-        Gizmos.DrawLine(transform.position, (Vector2)transform.position + (Vector2)(rot * Vector2.down));
+        Quaternion rot1 = Quaternion.AngleAxis(settings.slope_angle, Vector3.forward);
+        Quaternion rot2 = Quaternion.AngleAxis((-settings.slope_angle), Vector3.forward);
+        Gizmos.DrawLine(transform.position, (Vector2)transform.position + (Vector2)(rot1 * Vector2.down));
         Gizmos.DrawLine(transform.position, (Vector2)transform.position + (Vector2)(rot2 * Vector2.down));
+        #endregion
+
+        #region CEILING ANGLE BOUNDS
+        Quaternion rot3 = Quaternion.AngleAxis(settings.ceiling_angle, Vector3.forward);
+        Quaternion rot4 = Quaternion.AngleAxis((-settings.ceiling_angle), Vector3.forward);
+        Gizmos.DrawLine(transform.position, (Vector2)transform.position + (Vector2)(rot3 * Vector2.up));
+        Gizmos.DrawLine(transform.position, (Vector2)transform.position + (Vector2)(rot4 * Vector2.up));
         #endregion
 
         #region WALL JUMP ANGLE
         Gizmos.color = Color.blue;
-        float angle = wall_jump_angle * Mathf.RoundToInt(detection.x);
-        Quaternion rot3 = Quaternion.AngleAxis(angle, Vector3.forward);
-        Gizmos.DrawLine(transform.position, transform.position + (rot3 * Vector3.up));
+        float angle = settings.wall_jump_angle * Mathf.RoundToInt(detection.x);
+        Quaternion rot5 = Quaternion.AngleAxis(angle, Vector3.forward);
+        Gizmos.DrawLine(transform.position, transform.position + (rot5 * Vector3.up));
 
         #endregion
 
         #region PLAYER BOUNDS
-        Gizmos.color = new Color(1.0f, 0.0f, 0.85f, 0.4f);
-        Gizmos.DrawCube(transform.position + Player_Bounds.center, Player_Bounds.size);
-        Gizmos.color = new Color(0.0f, 0.0f, 0.0f, 1.0f);
-        Gizmos.DrawWireCube(transform.position + Player_Bounds.center, Player_Bounds.size);
+        if (show_bounds)
+        {
+            Gizmos.color = new Color(1.0f, 0.0f, 0.85f, 0.4f);
+            Gizmos.DrawCube(transform.position + Player_Bounds.center, Player_Bounds.size);
+            Gizmos.color = new Color(0.0f, 0.0f, 0.0f, 1.0f);
+            Gizmos.DrawWireCube(transform.position + Player_Bounds.center, Player_Bounds.size);
+        }
         #endregion
     }
 }
