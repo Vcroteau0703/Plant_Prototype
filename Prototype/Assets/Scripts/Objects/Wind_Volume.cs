a04ae1cb123232d29cb9555ca62d4963107a2e27
Tanner Cobb <tannercobb359@gmail.com>
2022-02-24 14:07:56 -0500

    FIX: Wind Volume now calculates from bottom

--- Prototype/Assets/Scripts/Objects/Wind_Volume.cs
+++ Prototype/Assets/Scripts/Objects/Wind_Volume.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class Wind_Volume : Action_Volume
 {
+    public Collider col;
+
     public float power = 1.0f;
     public float accel = 1.0f;
+
+    private void Awake()
+    {
+        col = TryGetComponent(out Collider c) ? c : null;
+    }
 
     private void OnEnable()
     {
@@ -14,8 +21,9 @@ public class Wind_Volume : Action_Volume
 
     public void Wind_Force(GameObject actor)
     {
-        float y_diff = Mathf.Clamp(actor.transform.position.y - transform.position.y, 0.01f, float.PositiveInfinity);
-        float force = Mathf.Pow(-Physics.gravity.y *  1 / y_diff * accel, power);
+        Vector3 start = transform.position - (col.bounds.extents.y * Vector3.up);
+        float y_diff = Mathf.Clamp(actor.transform.position.y - start.y, 0.01f, float.PositiveInfinity);
+        float force = Mathf.Pow(-Physics.gravity.y *  3 / y_diff * accel, power);
         Debug.Log(force);
         actor.GetComponent<Rigidbody>().AddForce(force * Vector2.up, ForceMode.Acceleration);
     }
