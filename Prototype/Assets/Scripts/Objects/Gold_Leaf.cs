904bd3454cdb36e3f03e1a2eef06e9f6beb8e265
Tanner Cobb <tannercobb359@gmail.com>
2022-04-14 14:44:23 -0400

    UPDATE: Gold Leaves Saving Fixed + States Now Save

--- Prototype/Assets/Scripts/Objects/Gold_Leaf.cs
+++ Prototype/Assets/Scripts/Objects/Gold_Leaf.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
+using UnityEditor;
+using System.Reflection;
 
 public class Gold_Leaf : Action_Volume
 {
@@ -12,9 +13,13 @@ public class Gold_Leaf : Action_Volume
         Player_Data data = SaveSystem.Load<Player_Data>("/Player/Player.data");
         if (data == null) { return; }
 
-        foreach (Gold_Leaf_Data g in data.goldLeaves)
+        int localID = ((int)transform.position.sqrMagnitude);
+
+        foreach (Gold_Leaf_Data saved in data.goldLeaves)
         {
-            if (GetInstanceID() == g.id)
+            Debug.Log(localID + " | " + saved.id);
+
+            if (localID == saved.id)
             {
                 Destroy(gameObject);
             }
@@ -59,6 +64,6 @@ public class Gold_Leaf_Data
     [SerializeField] public int id;
     public Gold_Leaf_Data(Gold_Leaf leaf)
     {
-        id = leaf.GetInstanceID();
+        id = ((int)leaf.transform.position.sqrMagnitude);
     }
 }
--- Prototype/Assets/Scripts/Player/State_Controller.cs
+++ Prototype/Assets/Scripts/Player/State_Controller.cs
@@ -2,11 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
 
-public class State_Controller : MonoBehaviour
+public class State_Controller : MonoBehaviour, ISavable
 {
     public bool isInvoking;
-    public List<State> states;
+    public State[] states;
+    public void Awake()
+    {
+        State_Controller_Data data = SaveSystem.Load<State_Controller_Data>("/Player/Abilities.data");
+        if(data != null)
+        {
+            int i = 0;
+            foreach(State_Data d in data.states_data)
+            {
+                states[i].active = d.active;
+                states[i].enabled = d.enabled;
+                states[i].name = d.name;
+                states[i].looping = d.looping;
+                i++;
+            }
+        }
+    }
     public void Start()
     {
         Enable_State("Idle");
@@ -81,6 +98,46 @@ public class State_Controller : MonoBehaviour
             }
         }
         return null;
+    }
+
+    public void Save()
+    {
+        State_Controller_Data data = new State_Controller_Data(states);
+        SaveSystem.Save(data, "/Player/Abilities.data");
+    }
+}
+
+[System.Serializable]
+public class State_Controller_Data
+{
+    public State_Data[] states_data;
+
+    public State_Controller_Data(State[] states)
+    {
+        states_data = new State_Data[states.Length];
+
+        int i = 0;
+        foreach(State s in states)
+        {
+            states_data[i] = new State_Data(s);
+            i++;
+        }
+    }
+}
+
+
+[System.Serializable]
+public class State_Data
+{
+    public string name;
+    public bool enabled = true;
+    public bool active;
+    public bool looping = false;
+    public State_Data(State state){
+        this.name = state.name;
+        this.enabled = state.enabled;
+        this.active = state.active;
+        this.looping = state.looping;
     }
 }
 
