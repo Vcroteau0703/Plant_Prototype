a598fe731ee7b3057df006a4e88349e94781efa7
Victoria Croteau <48254889+Vcroteau0703@users.noreply.github.com>
2022-04-19 14:37:45 -0400

    Fixed a bunch of audio bugs, closed off level 1 from level 2 to avoid save issues for pax party, and set up a bunch of new sfx stuff

--- Prototype/Assets/Scripts/Objects/Hazards/ParticleHazard.cs
+++ Prototype/Assets/Scripts/Objects/Hazards/ParticleHazard.cs
@@ -4,20 +4,40 @@ using UnityEngine;
 
 public class ParticleHazard : Hazard
 {
+    public AudioClip[] dripSounds;
     public ParticleSystem part;
+    AudioSource aS;
+
+    bool isPlayed;
+    int r;
 
     private void Awake()
     {
         part = GetComponent<ParticleSystem>();
+        aS = GetComponentInChildren<AudioSource>();
     }
 
-    private void OnParticleCollision(GameObject other)
+    private IEnumerator OnParticleCollision(GameObject other)
     {
-        if (other.gameObject.TryGetComponent(out IDamagable a))
+        if (!isPlayed)
         {
-            a.Damage(damage);
+            DripSFX();
+            isPlayed = true;
+            if (other.gameObject.TryGetComponent(out IDamagable a))
+            {
+                a.Damage(damage);
+            }
+            yield return new WaitForSeconds(0.2f);
+            isPlayed = false;
         }
 
+
     }
 
+    public void DripSFX()
+    {
+        r = Random.Range(0, dripSounds.Length);
+        aS.clip = dripSounds[r];
+        aS.Play();
+    }
 }
