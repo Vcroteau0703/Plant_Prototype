dfdf0be726b3f0bbc95dd3d69d763671aa3b9ed7
Victoria Croteau <48254889+Vcroteau0703@users.noreply.github.com>
2022-04-25 15:03:20 -0400

    updated dialog for levels 1 and 2, added voice acting to some of the dialog!

--- Prototype/Assets/DialogSFX.cs
+++ Prototype/Assets/DialogSFX.cs
@@ -1,20 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Yarn.Unity;
 
 public class DialogSFX : MonoBehaviour
 {
-    public AudioClip[] audioClips;
-    public AudioClip[] GJDLFG;
-    // Start is called before the first frame update
-    void Start()
+    public AudioClip[] spinnerClips;
+    public float sPitch;
+    public AudioClip[] groverClips;
+    public float gPitch;
+    public AudioClip[] frondClips;
+    public float fPitch;
+    public AudioClip[] aloeClips;
+    public float aPitch;
+    AudioSource aS;
+    int r;
+
+    private void Awake()
     {
-        
+        aS = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    [YarnCommand("PlayAudio")]
+    public void PlayAudio(string charName)
     {
-        
+        switch (charName)
+        {
+            case "spinner":
+                r = Random.Range(0, spinnerClips.Length);
+                aS.clip = spinnerClips[r];
+                aS.pitch = sPitch;
+                aS.Play();
+                break;
+            case "grover":
+                r = Random.Range(0, groverClips.Length);
+                aS.clip = groverClips[r];
+                aS.pitch = gPitch;
+                aS.Play();
+                break;
+            case "frond":
+                r = Random.Range(0, frondClips.Length);
+                aS.clip = frondClips[r];
+                aS.pitch = fPitch;
+                aS.Play();
+                break;
+            case "aloe":
+                r = Random.Range(0, aloeClips.Length);
+                aS.clip = aloeClips[r];
+                aS.pitch = aPitch;
+                aS.Play();
+                break;
+        }
+       
     }
 }
