609735bd69cb0a57850fb91aeaae6c24314a315b
Victoria Croteau <48254889+Vcroteau0703@users.noreply.github.com>
2022-04-27 13:39:14 -0400

    AudioManager now set up and audio is able to be changed from audio menu. Level 01 audio sources set up with mixers

--- Prototype/Assets/Scripts/FX/SFX/AudioManager.cs
+++ Prototype/Assets/Scripts/FX/SFX/AudioManager.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioMixer audioMixer;
+    public AudioSettings[] audioSettings;
+
+    public void SetExposedParam(int group)
     {
-        
+        audioMixer.SetFloat(audioSettings[group].exposedParam, audioSettings[group].slider.value);
+        PlayerPrefs.SetFloat(audioSettings[group].exposedParam, audioSettings[group].slider.value);
     }
 
-    // Update is called once per frame
-    void Update()
+}
+[System.Serializable]
+public class AudioSettings
+{
+    public Slider slider;
+    public string exposedParam;
+
+    public AudioSettings()
     {
-        
+        slider.value = PlayerPrefs.GetFloat(exposedParam);
     }
 }
